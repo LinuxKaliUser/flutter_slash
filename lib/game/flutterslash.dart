0d22fcdc1c34d61479b68eb2ac8a95323f3a6425
Liam Metzger <liam.metzger@bluewin.ch>
2025-02-17 15:08:06 +0100

    feat: Add background map

--- lib/game/flutterslash.dart
+++ lib/game/flutterslash.dart
@@ -2,28 +2,28 @@ import 'dart:ui';
 
 import 'package:flame/events.dart';
 import 'package:flame/game.dart';
+import 'package:flame_tiled/flame_tiled.dart';
 
 import 'package:flutter_slash/game/player.dart';
 
 class FlutterSlashGame extends FlameGame
     with HasKeyboardHandlerComponents, PointerMoveCallbacks, DragCallbacks {
   late PlayerCharacter player;
+  late TiledComponent tiledMap;
+
 
   @override
   Future<void> onLoad() async {
     await super.onLoad();
 
+    tiledMap = await TiledComponent.load('flutter-slash.tmx', Vector2.all(32));
     player = PlayerCharacter();
 
+    world.add(tiledMap);
     world.add(player);
 
-    // TODO: Discuss the possibilty of changing the camera to not
-    // have infinite speed so that player isn't always perfectly centered.
     camera.follow(player);
   }
-
-  //@override
-  //Color backgroundColor() => const Color(0xFFC4A484);
 
   @override
   void onPointerMove(PointerMoveEvent event) {
